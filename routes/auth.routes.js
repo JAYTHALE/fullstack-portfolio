c4f5ace2f2813c9cd8236a8b2c621995cb25fa91
jaythale <jaythale@123.com>
2024-09-03 14:50:50 +0530

    update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router()
 const auth = require("../controller/auth.controller")
 
 router
-    .post("./register", auth.registerUser)
-    .post("./login", auth.logoutUser)
-    .post("./logout", auth.logoutUser)
+    .post("/register", auth.registerUser)
+    .post("/login", auth.logoutUser)
+    .post("/logout", auth.logoutUser)
 
 module.exports = router
